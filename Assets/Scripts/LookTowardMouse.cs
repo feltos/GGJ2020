74ac4607814c57fa55e4e3e253cf8e9dbbb88a38
guillaume volgyesi <guillaumevolgyesi@gmail.com>
2020-01-31 20:53:33 +0100

    movement,rotation and attack done

---
+++ Assets/Scripts/LookTowardMouse.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookTowardMouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mousepos = Input.mousePosition;
+        var playerpos = Camera.main.WorldToScreenPoint(transform.position);
+        var dir = mousepos - playerpos;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(-angle, Vector3.up);
+    }
+
+    /* //Get the Screen positions of the object
+     Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+
+     //Get the Screen position of the mouse
+     Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+     //Get the angle between the points
+     float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+
+     //Ta Daaa
+     transform.rotation = Quaternion.Euler(new Vector3(0f, angle, 0f));
+
+ float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+ {
+     return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+ }*/
+}
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float horizontal;
+    float vertical;
+
+    Rigidbody body;
+    Vector3 mouseDirection;
+    Vector2 movement;
+    [SerializeField]float speed;
+
+    Vector3 mousePosition;
+    float mouseInput;
+
+    [SerializeField] BoxCollider weaponKey;
+    float hitTimer = 0.0f;
+    float hitPeriod = 0.05f;
+    bool hit = false;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        weaponKey.enabled = false;
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            hit = true;
+            hitTimer = 0.0f;
+            weaponKey.enabled = true;
+        }
+        Debug.Log(hit);
+    }
+
+    void FixedUpdate()
+    {
+        body.velocity = new Vector3(speed * horizontal, 0, speed * vertical); ;
+        if(hit)
+        {
+            hitTimer += Time.deltaTime;
+        }
+        if (hitTimer > hitPeriod)
+        {
+            weaponKey.enabled = false;
+            hit = false;
+        }
+    }
+}
