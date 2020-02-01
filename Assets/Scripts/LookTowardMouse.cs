416fb82fbe09d624950f23c52b653a4473621cf4
damyankay <damyan@damyankay.com>
2020-02-02 00:35:56 +0100

    merge

--- Assets/Scripts/LookTowardMouse.cs
+++ Assets/Scripts/LookTowardMouse.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class LookTowardMouse : MonoBehaviour
 {
+    [SerializeField]Camera camera;
     // Update is called once per frame
     void Update()
     {
         var mousepos = Input.mousePosition;
-        var playerpos = Camera.main.WorldToScreenPoint(transform.position);
+        var playerpos = camera.WorldToScreenPoint(transform.position);
         var dir = mousepos - playerpos;
         var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(-angle, Vector3.up);
