1a1d5ad267475396d1e870da298492d65dc42b2d
damyankay <damyan@damyankay.com>
2020-02-02 01:10:02 +0100

    merge

---
+++ Assets/Hit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButton("Fire1"))
+        {
+            GetComponent<Animation>().Play("Hit");
+
+        }
+        else {
+
+            GetComponent<Animation>().Play("Run");
+            GetComponent<Animation>().wrapMode = WrapMode.Loop;
+
+
+        }
+
+    }
+}
